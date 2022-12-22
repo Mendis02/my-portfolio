25caf66bf375db51a6ce59bd374e437021d3d14b
thanura mendis <thanuramendis02@gmail.com>
2022-12-22 13:27:56 +0530

    newly

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+export default function Footer(){
+    return(
+        <nav className="footer">
+            {/* <a href="https://www.facebook.com/profile.php?id=100079685223110">
+            <img src="https://play-lh.googleusercontent.com/ccWDU4A7fX1R24v-vvT480ySh26AYp97g1VrIB_FIdjRcuQB2JP2WdY7h_wVVAeSpg=w240-h480-rw" alt="facebook" />
+            </a> */}
+
+            <a href="https://www.instagram.com/thanura.m/"><button>instagram</button></a>
+            <a href="https://github.com/Mendis02"><button>github</button></a>
+        </nav>    
+    )
+}
---
+++ src/components/Info.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+export default function Info(){
+    return(
+        <div>
+            <img src= "https://cdn.dribbble.com/users/2987571/screenshots/7084912/media/2c72c0b54f0ae2d09cb505beb00de0e6.png" className = "nav--image"/>
+            <h1 className="name" >Thanura Mendis</h1>
+            <h4 className="frontend"> Frontend Developer</h4>
+            {/* <a href="https://www.google.com/"><button>GMAIL</button></a> */}
+        </div>
+    )
+}
